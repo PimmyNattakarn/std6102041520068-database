eca1287dbf55934e3d8996bcad694ae1da5aae58
PimmyNattakarn <48780876+PimmyNattakarn@users.noreply.github.com>
2019-04-11 08:25:12 +0700

    Update registration.sql

--- registration.sql
+++ registration.sql
@@ -3,8 +3,7 @@ DROP TABLE IF EXISTS `registration`;
 CREATE TABLE `registration` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `client_id` int(11) DEFAULT NULL,
-  `branch_id` int(11) DEFAULT NULL,
-  `staff_id` int(11) DEFAULT NULL,
   `dateJoined` date DEFAULT NULL,
+  `property_for_rent_id` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
