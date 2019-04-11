b6b57834c304378608ec8a2be0dbb5a8a6ad1bf1
PimmyNattakarn <48780876+PimmyNattakarn@users.noreply.github.com>
2019-04-11 08:26:53 +0700

    Update property_for_rent.sql

--- property_for_rent.sql
+++ property_for_rent.sql
@@ -3,9 +3,6 @@ DROP TABLE IF EXISTS `property_for_rent`;
 CREATE TABLE `property_for_rent` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `property_no` varchar(5) DEFAULT NULL,
-  `street` text,
-  `city` text,
-  `postcode` varchar(10) DEFAULT NULL,
   `type` varchar(50) DEFAULT NULL,
   `rooms` int(5) DEFAULT NULL,
   `rent` float(7,2) DEFAULT NULL,
